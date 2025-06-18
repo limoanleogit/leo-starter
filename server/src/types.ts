9dddc9c2dbe91d8ba988327eccea9a3904ea7296
levrai <limoanleo@gmail.com>
2025-06-18 19:23:41 +0100

    refactor: update configs server

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,4 +1,5 @@
-import { InputType, Field } from "type-graphql";
+import { Length } from "class-validator";
+import { InputType, Field, ObjectType } from "type-graphql";
 
 @InputType()
 export class UserInput {
@@ -8,4 +9,53 @@ export class UserInput {
   email!: string;
   @Field(() => String)
   password!: string;
+  @Field(() => String)
+  firstname!: string;
+  @Field(() => String)
+  lastname!: string;
+}
+
+@InputType()
+export class UserLogin {
+
+  @Field(() => String)
+  @Length(1, 255)
+  username!: string;
+
+  @Field(() => String)
+  password!: string;
+
+}
+
+@ObjectType()
+export class FieldError {
+  @Field(() => String)
+  field: string;
+  @Field(() => String)
+  message: string;
+}
+
+@ObjectType()
+export class LoginResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => String, { nullable: true })
+  token?: string;
+
+  @Field(() => String, { nullable: true })
+  userId?: string;
+}
+
+@InputType()
+export class QuestionInput {
+  @Field(() => String)
+  film: string;
+
+  @Field(() => String)
+  acteur: string;
+
+  @Field(() => Boolean)
+  isFilmChoice: boolean;
+
 }
